200fe3e6022d50a7acc36572a48404a25c2cafc6
João Vitor Toppa <jvtoppa@gmail.com>
2025-04-04 23:27:38 -0300

    Update sensors.py

--- sensors.py
+++ sensors.py
@@ -1,7 +1,7 @@
 import smbus
-import time
 import board
 import busio
+from time import sleep
 import adafruit_ccs811
 import adafruit_bme280
 
@@ -13,11 +13,14 @@ class Accelerometer:
         self.devConfg = deviceConfig
         self.bus = i2cBus
         self.pwrMgr = powerManager
+        self.alpha = 0.9
+        self.last = [0.0, 0.0, 0.0]
+        self.hp = [0.0, 0.0, 0.0]
         self.initialize()
     
     def initialize(self):
         self.bus.write_byte_data(self.addr, self.pwrMgr, 0x00)
-        time.sleep(0.1)
+        sleep(0.1)
         self.bus.write_byte_data(self.addr, self.devConfg, 0x00)
         
     def readRaw(self, addr):
@@ -27,11 +30,20 @@ class Accelerometer:
         if value > 32767:
             value -= 65536
         return value
-    
+
     def readVibration(self):
-        acc_x = self.readRaw(self.xH)
-        Ax = acc_x / 16384.0
-        return Ax
+        base_addr = self.xH
+        axes = [0.0, 0.0, 0.0]
+        for i in range(3):
+            raw = self.readRaw(base_addr + 2*i)
+            g = raw / 16384.0
+            self.hp[i] = self.alpha * (self.hp[i] + g - self.last[i])
+            self.last[i] = g
+            axes[i] = self.hp[i] * 9.81
+        vib_total = (axes[0]**2 + axes[1]**2 + axes[2]**2) ** 0.5
+        return vib_total
+
+
 
 class SensorProbe:
     def __init__(self, ccs811, mpu9250, bme280):
@@ -44,7 +56,7 @@ class SensorProbe:
             try:
                 return self.vo2.eco2
             except Exception as e:
-                print("Could not read eCO2 - " + e)
+                print("Could not read eCO2 - " , e)
                 return "N/A"
         else:
             return "N/A"
@@ -54,7 +66,7 @@ class SensorProbe:
             try:
                 return self.vo2.tvoc
             except Exception as e:
-                print("Could not read TVOC - " + e)
+                print("Could not read TVOC - ", e)
                 return "N/A"
         else:
             return "N/A"
@@ -62,9 +74,9 @@ class SensorProbe:
     def readHumidity(self):
         if self.humTemp:
             try:
-                return self.humTemp.relative_humidity
+                return self.humTemp.humidity
             except Exception as e:
-                print("Could not read humidity values - " + e)
+                print("Could not read humidity values - ", e)
                 return "N/A"
         else:
             return "N/A"
@@ -74,7 +86,7 @@ class SensorProbe:
             try:
                 return self.humTemp.temperature
             except Exception as e:
-                print("Could not read temperature - " + e)
+                print("Could not read temperature - ", e)
                 return "N/A"
         else:
             return "N/A"
@@ -85,7 +97,7 @@ class SensorProbe:
             try:
                 return self.accel.readVibration()
             except Exception as e:
-                print("Could not read vibration - " + e)
+                print("Could not read vibration - ", e)
                 return "N/A"
         else:
             return "N/A"
