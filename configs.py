787591d088df9d9faafbcd6fcebd36533f392325
João Vitor Toppa <jvtoppa@gmail.com>
2025-04-05 01:24:43 -0300

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,4 +1,17 @@
+#PROBE 1
+
 DEVICE_ADDRESS = 0x68
 PWR_MGMT_1 = 0x6B
 ACCEL_CONFIG = 0x1C
 ACCEL_XOUT_H = 0x3B
+
+#CAMERA
+
+width = 300
+height = 300
+fps = 120
+
+
+#TABLES
+
+path_to_folder = '' #Change accordingly
